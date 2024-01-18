ddf5f6fd44dd6d53e1b5b3364d977393484213ab
gemario <gmario.sh@gmail.com>
2024-01-18 12:53:33 -0300

    desafio finalizado!

---
+++ desafio-rocketCoffee/script.js
@@ -0,0 +1,11 @@
+function clickMenu() {
+  if (itens.style.display === "block") {
+    itens.style.display = "none";
+    main.style.display = "block";
+    blur2.style.display = "block";
+  } else {
+    itens.style.display = "block";
+    main.style.display = "none";
+    blur2.style.display = "none";
+  }
+}
